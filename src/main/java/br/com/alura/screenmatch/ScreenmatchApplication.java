c21cef1d8b4b553f31acf68cef44dab45c0ea2a3
walace nascimento <wlacens@hotmail.com>
2024-05-31 18:06:28 -0300

    colocando as coisas no lugar, aula bucando dados completo da série

--- src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -1,16 +1,9 @@
 package br.com.alura.screenmatch;
 
-import br.com.alura.screenmatch.model.DadosEpisodio;
-import br.com.alura.screenmatch.model.DadosSerie;
-import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.services.ConsumoApi;
-import br.com.alura.screenmatch.services.ConverteDados;
+import br.com.alura.screenmatch.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class ScreenmatchApplication  implements CommandLineRunner {
@@ -21,38 +14,9 @@ public class ScreenmatchApplication  implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Primeiro projeto Spring sem web");
-
-		String apiKey = "e465ef95";
-		ConsumoApi consumoApi = new ConsumoApi();
-		// chamando uma API
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=e465ef95");
-		// "https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=e465ef95"
-
-		// Chamando outra api
-//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
-//		System.out.println(json);
-
-		System.out.println(json);
-		ConverteDados conversor = new ConverteDados();
-		DadosSerie dados = conversor.obterDados(json, DadosSerie.class);
-		System.out.println(dados);
-
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=1&episode=2&apikey=e465ef95");
-		DadosEpisodio dadosEpisodio = conversor.obterDados(json, DadosEpisodio.class);
-		System.out.println(dadosEpisodio);
-
-		// Criando uma lista para adicionar as temporadas
-		List<DadosTemporada> temporadas = new ArrayList<>();
-
-		// Percorrendo todas as temporadas com laço FOR
-		for (int i = 1; i <= dados.totalTemporadas() ; i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&season=" + i + "&apikey=e465ef95");
-			DadosTemporada dadosTemporada = conversor.obterDados(json, DadosTemporada.class);
-			temporadas.add(dadosTemporada);
-		}
-
-		temporadas.forEach(System.out::println);
+		// System.out.println("Primeiro projeto Spring sem web");
+		Principal principal = new Principal();
+		principal.exibeMenu();
 
 	}
 }
