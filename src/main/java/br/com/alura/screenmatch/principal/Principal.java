1de55955a56e3684a9232ab4673384dec49633ca
walace nascimento <wlacens@hotmail.com>
2024-06-03 20:22:01 -0300

    •Criando um mapa com dados das temporadas

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -122,6 +122,12 @@ public class Principal {
 //                                "Data lançamento: " + e.getDataLancamento().format(formatador)
 //                ));
 
+        // Criando um mapa com dados por temporada
+        Map<Integer, Double> avaliacoesPorTemporada = episodios.stream()
+                .filter(e -> e.getAvaliacao() > 0.0)
+                .collect(Collectors.groupingBy(Episodio::getTemporada,
+                        Collectors.averagingDouble(Episodio::getAvaliacao)));
 
+        System.out.println(avaliacoesPorTemporada);
     }
 }
